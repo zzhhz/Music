e9fbab1ed92ec69923497c9841fe1c03b8df5309
zzhhz <zzh_hz@126.com>
2016-05-18 14:58:19 +0800

    zzh:全局文件

---
+++ app/src/main/java/com/zzh/music/MusicApplication.java
@@ -0,0 +1,19 @@
+package com.zzh.music;
+
+import android.app.Application;
+
+/**
+ * Created by ZZH on 16/5/18.
+ *
+ * @Date: 16/5/18
+ * @Email: zzh_hz@126.com
+ * @QQ: 1299234582
+ * @Author: zzh
+ * @Description:
+ */
+public class MusicApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/zzh/music/MusicConstants.java
@@ -0,0 +1,13 @@
+package com.zzh.music;
+
+/**
+ * Created by ZZH on 16/5/18.
+ *
+ * @Date: 16/5/18
+ * @Email: zzh_hz@126.com
+ * @QQ: 1299234582
+ * @Author: zzh
+ * @Description: 保存常量
+ */
+public class MusicConstants {
+}
